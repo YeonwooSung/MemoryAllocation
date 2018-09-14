4b63b54f2eb9d6a41e98d737ee00b3d3c79e44ff
YeonwooSung <neos960518@gmail.com>
2018-09-14 19:24:12 +0900

    start implementing the myalloc function

--- mymalloc.c
+++ mymalloc.c
@@ -1,3 +1,20 @@
-#include <stdio.h>
-
 #include "mymalloc.h"
+
+/**
+ * The aim of this function is to allocate the memory dynamically.
+ *
+ * @param size the size of the memory
+ * @return ptr the starting point of the allocated memory.
+ */
+void *myalloc(int size) {
+    return NULL;
+}
+
+/**
+ * This function frees the dynamically allocated memory.
+ *
+ * @param ptr the pointer that points the starting point of the address that should be freed.
+ */
+void myfree(void *ptr) {
+    //
+}
